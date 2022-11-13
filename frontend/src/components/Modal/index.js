5faa6db324396b352fcc6e97e230779385786bdc
Rafael Nascimento <rafael.nascimento@techsavvy.com.br>
2022-11-13 14:48:03 -0300

    feat: finished component modal

---
+++ frontend/src/components/Modal/index.js
@@ -0,0 +1,41 @@
+import ReactDOM from 'react-dom';
+import { PropTypes } from 'prop-types';
+
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
+
+export default function Modal({ danger }) {
+  return ReactDOM.createPortal(
+    <Overlay>
+      <Container danger={danger}>
+        <h1>
+          Titulo do modal
+        </h1>
+
+        <p>
+          Texto do modal
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button" danger={danger}>
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>,
+    document.getElementById('modal-root'),
+  );
+}
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
